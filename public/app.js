aaa9028afd21fc268e355b073b59990e62ee1ea5
Eirik Stien <eirikstien@gmail.com>
2016-07-25 19:07:34 +0200

    working chat

--- public/app.js
+++ public/app.js
@@ -1,1 +1,37 @@
-console.log('hello Annistyt!');
+var socket = io();
+var messagesNode = $('.messages');
+var messages = [];
+
+$(() => {
+  var sendButton = $('.send-button');
+  var inputField = $('#messageField');
+  sendButton.click(() => {
+    var text = inputField.val();
+    if(text.trim() !== '') {
+      socket.emit('send message', {text});
+      inputField.val('');
+    }
+  });
+  inputField.on('keyup', event => {
+    if(event.keyCode == 13) {
+      sendButton.click();
+    }
+  })
+});
+
+
+var updateMessageList = () => {
+  messagesNode.html('');
+  messages.forEach(message => {
+    $('<li>').text(message.text).appendTo(messagesNode);
+  });
+};
+
+socket.on('message', data => {
+  messages.push(data);
+  updateMessageList();
+  // alert(`fikk melding fra bruker: ${data.text}`);
+
+});
+
+
--- server.js
+++ server.js
@@ -7,7 +7,21 @@ app.use(bodyParser.json());
 
 app.use(express.static('public', {maxAge: 0}));
 
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+
+io.on('connection', socket => {
+  
+  socket.on('send message', data => {
+
+    io.emit('message', data);
+
+  });
+
+});
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+http.listen(port, () => {
   console.info(`Server now live on http://localhost:${port}`)
 });
